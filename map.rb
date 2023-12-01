99b004b9cd5ad11e7d1e947460987bf15adb92dc
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-01 13:54:08 +0100

    Map class done

--- map.rb
+++ map.rb
@@ -21,6 +21,23 @@ class Map
         end
     end
 
+
+    def get(key)
+        @map.each do |pair|
+            if pair[0] == key
+                return pair
+            end
+        end
+    end
+
+    def delete(key)
+        @map.each_with_index do |pair, idx|
+            if pair[0] == key
+                @map.delete_at(idx)
+            end
+        end
+    end
+
     def show
         return @map
     end
